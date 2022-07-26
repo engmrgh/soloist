3f430db66eeb0d86d0d974eba965a05637de42ba
Mohammadreza Ghofrani <ghofrani132@gmail.com>
2022-07-26 08:10:04 +0000

    remove logging validation examples.

--- train.py
+++ train.py
@@ -287,7 +287,6 @@ def train():
             batch = tuple(input_tensor.to(args.device)
                           for input_tensor in batch)
             input_ids, mc_token_ids, lm_labels, mc_labels, token_type_ids, attention_mask = batch
-            logger.info(tokenizer.decode(input_ids[0, -1, :].tolist()))
             # if we dont send labels to model, it doesnt return losses
             lm_logits, mc_logits, *_ = model(
                 input_ids, token_type_ids=token_type_ids, mc_token_ids=mc_token_ids,
